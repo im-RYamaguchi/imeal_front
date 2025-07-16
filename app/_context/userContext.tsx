02a5423d7dd7a0a7fc1471294277b752a97c3825
im-RYamaguchi <r.yamaguchi@infomart.co.jp>
2025-07-16 10:05:46 +0900

    ローカルストレージにユーザー情報保存

--- app/_context/userContext.tsx
+++ app/_context/userContext.tsx
@@ -1,9 +1,9 @@
 "use client"
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { UserData } from "../_interfaces/dto/response/UserData";
 import { LoginFormData } from "../_interfaces/dto/request/LoginFormData";
-import { useRouter } from "next/navigation";
 import * as auths from "../_utils/api/auths";
+import { extractErrorMessages } from "../_utils/errorHandler";
 
 interface UserContextType{
   user: UserData | null;
@@ -12,8 +12,10 @@ interface UserContextType{
   logout: () => void;
 }
 
+// コンテキスト作成
 const UserContext = createContext<UserContextType | undefined>(undefined);
 
+// コンテキストプロバイダー
 const UserProvider = ({children}: {children: React.ReactNode}) => {
     // ログイン中のユーザー
   const [user, setUser] = useState<UserData | null>(null);
@@ -27,7 +29,8 @@ const UserProvider = ({children}: {children: React.ReactNode}) => {
       const user = await auths.login(loginForm);
       // ユーザーセット
       setUser(user);
-      
+      // ローカルストレージに保存
+      localStorage.setItem('user', JSON.stringify(user));
     }catch(error){
       throw error;
     }
@@ -38,12 +41,41 @@ const UserProvider = ({children}: {children: React.ReactNode}) => {
     try{
       // ログアウトAPIリクエスト
       await auths.logout();
-      // ユーザーセット解除
+      // ユーザーリセット
       setUser(null);
+      // ローカルストレージリセット
+      localStorage.removeItem('user');
     }catch(error){
       throw error;
     }
   }
+
+  // マウント時実行
+  useEffect(() => {
+    setIsUserLoading(true);
+    // ローカルストレージのユーザー情報取得
+    const storedUser = localStorage.getItem('user');
+    // ユーザーが保存されていた場合
+    if(storedUser){
+      try{
+        // JSON → オブジェクト型変換
+        const parsedUser = JSON.parse(storedUser);
+        // 型が正しいかチェック
+        if(
+          typeof parsedUser === 'object' &&
+          parsedUser !== null &&
+          'id' in parsedUser &&
+          'name' in parsedUser
+        ){
+          setUser(parsedUser);
+        }
+      }catch(error){
+        // エラーメッセージコンソール表示
+        console.error(extractErrorMessages(error));
+      }
+    }
+    setIsUserLoading(false);
+  },[])
 
   return(
     <UserContext.Provider value={{user, isUserLoading, login, logout}}>
