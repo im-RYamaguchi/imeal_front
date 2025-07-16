bc27e8a599f119c1812a008ba45359a3994a9768
im-RYamaguchi <r.yamaguchi@infomart.co.jp>
2025-07-16 14:01:22 +0900

    ヘッダーにログインユーザー名表示

--- app/_components/common/header/index.tsx
+++ app/_components/common/header/index.tsx
@@ -3,18 +3,20 @@ import ClickButton from "../../button/clickButton";
 import LinkButton from "../../button/linkButton";
 import SelectButton from "../../button/selectButton";
 import { PAGE_PATHS } from "@/app/_constants/pagePath";
-import { APP_NAME, HANDLE_NAME, PAGE_LABELS, PAGE_TITLES } from "@/app/_constants/pageText";
+import { APP_NAME, HANDLE_NAME, PAGE_LABELS, PAGE_TITLES, WELCOME_MESSAGE } from "@/app/_constants/pageText";
 import styles from './Header.module.css';
 import { BaseData } from "@/app/_interfaces/dto/response/BaseData";
 import { useHeader } from "@/app/_hocks/useHeader";
+import { UserData } from "@/app/_interfaces/dto/response/UserData";
 
 interface HeaderProps{
   bases: BaseData[];
   base: BaseData;
+  user: UserData | null;
 }
 
 // ヘッダー
-const Header = ({bases, base}: HeaderProps) => {
+const Header = ({bases, base, user}: HeaderProps) => {
   // ヘッダーコンテキスト
   const {options, handleChangeBase, handleLogout} = useHeader({bases});
 
@@ -31,6 +33,14 @@ const Header = ({bases, base}: HeaderProps) => {
         </div>
       </div>
 
+        {/* ログインユーザー表示 */}
+        {user && (
+          <div>
+            <p>{WELCOME_MESSAGE(user.name)}</p>
+          </div>
+          )
+        }
+        
       <div className={styles.rightArea}>
         {/* グルメ・口コミ投稿ページ遷移 */}
         <LinkButton href={PAGE_PATHS.CREATE_SHOP_AND_REVIEW(base.id)} text={PAGE_TITLES.CREATE_SHOP_AND_REVIEW}  />
