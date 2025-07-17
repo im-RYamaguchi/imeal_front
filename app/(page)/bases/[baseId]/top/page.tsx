6098c5717e7c14663679ba191ad3806e20dc67be
im-RYamaguchi <r.yamaguchi@infomart.co.jp>
2025-07-17 15:36:04 +0900

    マップ、口コミの表示

--- app/(page)/bases/[baseId]/top/page.tsx
+++ app/(page)/bases/[baseId]/top/page.tsx
@@ -1,12 +1,27 @@
 import { PAGE_SUB_TITLES, PAGE_TITLES } from "@/app/_constants/pageText";
+import ShopMap from "@/app/_components/shopMap";
+
+import styles from "./TopPage.module.css";
+import { mockReviews, mockShop, mockShops } from "@/___tests___/mocks/data";
+import Reviews from "@/app/_components/reviews";
 
 // トップページ
 const TopPage = () => {
   return(
     <>
       <h1>{PAGE_TITLES.TOP}</h1>
-      <h2>{PAGE_SUB_TITLES.TOP.SHOPS_MAP}</h2>
-      <h2>{PAGE_SUB_TITLES.TOP.RECENT_REVIEWS}</h2>
+
+      <div className={styles.content}>
+        <div>
+          <h2>{PAGE_SUB_TITLES.TOP.SHOPS_MAP}</h2>
+          <ShopMap base={mockShop.base} shops={mockShops} />
+        </div>
+
+        <div>
+          <h2>{PAGE_SUB_TITLES.TOP.RECENT_REVIEWS}</h2>
+          <Reviews reviews={mockReviews} direction={'column'}/>
+        </div>
+      </div>
     </>
   );
 };
