0ce08dad232099f93ad1503e9252e3532f0b74c4
im-RYamaguchi <r.yamaguchi@infomart.co.jp>
2025-07-23 10:57:06 +0900

    投稿する飲食店名表示

--- app/(page)/bases/[baseId]/shops/[shopId]/reviews/create/page.tsx
+++ app/(page)/bases/[baseId]/shops/[shopId]/reviews/create/page.tsx
@@ -1,18 +1,34 @@
 "use client"
 
-import { PAGE_TITLES } from "@/app/_constants/pageText";
+import { GETTING_ERROR, PAGE_LABELS, PAGE_TITLES } from "@/app/_constants/pageText";
 import { useCreateReview } from "@/app/_hocks/useReview";
 import { base } from "@/app/_test/testData";
 import Form from "@/app/_components/form";
 
+import styles from './CreateReviewPage.module.css';
+import Loading from "@/app/_components/common/loading";
+import ErrorMessage from "@/app/_components/error/errorMessage";
 
 // 口コミ投稿ページ
 const CreateReviewPage = () => {
 
-  const {handleCreateReview,handleSubmit,serverErrorMessages,register,errors,inputs} = useCreateReview({base: base});
+  // 口コミ投稿フック
+  const {shop, isShopLoading, handleCreateReview,handleSubmit,serverErrorMessages,register,errors,inputs} = useCreateReview({base: base});
+
+  // ローディング中の場合
+  if(isShopLoading){
+    return <Loading />
+  }
+
+  // 飲食店が空の場合
+  if(shop === null){
+    return <ErrorMessage errorMessage={GETTING_ERROR(PAGE_LABELS.SHOP.VARIABLE_NAME)} />
+  }
+
   return (
-    <div>
+    <div className={styles.reviewForm}>
       <h1>{PAGE_TITLES.CREATE_REVIEW}</h1>
+      <h2>{PAGE_LABELS.SHOP.NAME}：{shop.name}</h2>
       <Form
         onSubmit={handleCreateReview}
         handleSubmit={handleSubmit}
