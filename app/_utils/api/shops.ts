e5d0ab72b5eadb19ce06b4a59830bbd140ba6aac
im-RYamaguchi <r.yamaguchi@infomart.co.jp>
2025-07-23 15:30:42 +0900

    APIの型修正

--- app/_utils/api/shops.ts
+++ app/_utils/api/shops.ts
@@ -1,15 +1,14 @@
 import { ShopData } from "@/app/_interfaces/dto/response/ShopData";
 import { api } from "./api";
 import { SHOPS_URL } from "./apiUrl";
-import { ShopApiResponse, ShopsApiResponse, ShopWithReviewsAPIResponse } from "@/app/_interfaces/api/shops";
 import { ShopWithReviewsData } from "@/app/_interfaces/dto/response/ShopWithReviewsData";
 import { ShopFormData } from "@/app/_interfaces/dto/request/ShopFormData";
 
 // 飲食店取得（id）
 export const getShopById = async (shopId: number): Promise<ShopData> => {
   try{
-    const response = await api.get<ShopApiResponse>(SHOPS_URL.GET_BY_ID(shopId));
-    return response.data.shop;
+    const response = await api.get<ShopData>(SHOPS_URL.GET_BY_ID(shopId));
+    return response.data;
   }catch(error){
     throw error;
   }
@@ -18,8 +17,8 @@ export const getShopById = async (shopId: number): Promise<ShopData> => {
 // 飲食店一覧取得（baseIdでフィルター可能）
 export const getShops = async (baseId?: number): Promise<ShopData[]> => {
   try{
-    const response = await api.get<ShopsApiResponse>(SHOPS_URL.GET(baseId));
-    return response.data.shops;
+    const response = await api.get<ShopData[]>(SHOPS_URL.GET(baseId));
+    return response.data;
   }catch(error){
     throw error;
   }
@@ -28,8 +27,8 @@ export const getShops = async (baseId?: number): Promise<ShopData[]> => {
 // 飲食店とその口コミ一覧取得
 export const getShopWithReviews = async (shopId: number): Promise<ShopWithReviewsData> => {
   try{
-    const response = await api.get<ShopWithReviewsAPIResponse>(SHOPS_URL.GET_WITH_REVIEWS(shopId));
-    return response.data.shop;
+    const response = await api.get<ShopWithReviewsData>(SHOPS_URL.GET_WITH_REVIEWS(shopId));
+    return response.data;
   }catch(error){
     throw error;
   }
@@ -38,8 +37,8 @@ export const getShopWithReviews = async (shopId: number): Promise<ShopWithReview
 // 飲食店作成
 export const createShop = async (shopForm :ShopFormData): Promise<ShopData> => {
   try{    
-    const response = await api.post<ShopApiResponse>(SHOPS_URL.POST, shopForm);
-    return response.data.shop;
+    const response = await api.post<ShopData>(SHOPS_URL.POST, shopForm);
+    return response.data;
   }catch(error){
     throw error;
   }
@@ -48,8 +47,8 @@ export const createShop = async (shopForm :ShopFormData): Promise<ShopData> => {
 // 飲食店編集
 export const editShop = async (shopForm: ShopFormData, shopId: number): Promise<ShopData> => {
   try{
-    const response = await api.put<ShopApiResponse>(SHOPS_URL.PUT(shopId), shopForm);
-    return response.data.shop;
+    const response = await api.put<ShopData>(SHOPS_URL.PUT(shopId), shopForm);
+    return response.data;
   }catch(error){
     throw error;
   }
