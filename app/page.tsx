fc64200d4f75f4d92c7dfd05e2bf2c6bde37f8b0
im-RYamaguchi <r.yamaguchi@infomart.co.jp>
2025-07-24 14:28:23 +0900

    不要なデータ削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
-import Link from "next/link";
-import { PAGE_PATHS } from "./_constants/pagePath";
 import { PAGE_TITLES } from "./_constants/pageText";
-import { base } from "./_test/testData";
-
 
 // ホームページ
 const HomePage = () => {
   return(
     <>
       <h1>{PAGE_TITLES.HOME}</h1>
-      <h2><Link href={PAGE_PATHS.TOP(base.id)}>{PAGE_TITLES.TOP}</Link></h2>
     </>
   );
 };
