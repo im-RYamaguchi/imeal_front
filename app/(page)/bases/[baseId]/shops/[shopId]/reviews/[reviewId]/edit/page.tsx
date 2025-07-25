3c4969afe12925c5f9ed3441b59f3b618eeb9161
IMshirai <m.shirai@infomart.co.jp>
2025-07-25 09:25:15 +0900

    口コミ画面表示

--- app/(page)/bases/[baseId]/shops/[shopId]/reviews/[reviewId]/edit/page.tsx
+++ app/(page)/bases/[baseId]/shops/[shopId]/reviews/[reviewId]/edit/page.tsx
@@ -1,7 +1,41 @@
+"use client"
+
+import { GETTING_ERROR, PAGE_LABELS, PAGE_TITLES } from "@/app/_constants/pageText";
+import { useEditReview } from "@/app/_hocks/useEditReview";
+import Form from "@/app/_components/form";
+import styles from './EditReviewPage.module.css';
+import ErrorMessage from "@/app/_components/error/errorMessage";
+import Loading from "@/app/_components/common/loading";
+
 // 口コミ編集ページ
 const EditReviewPage = () => {
-  return(
-    <h1>口コミ編集ページ</h1>
+
+  //口コミ編集フック
+  const { shop,isShopLoading,handleEditReview, handleSubmit, serverErrorMessages, register, errors, fields } = useEditReview();
+
+  //ローディング中の場合
+  if(isShopLoading){
+    return<Loading/>
+  }
+
+  // 飲食店が空の場合
+  if(shop === null){
+    return <ErrorMessage errorMessage={GETTING_ERROR(PAGE_LABELS.SHOP.VARIABLE_NAME)} />
+  }
+  
+  return (
+    <div className={styles.reviewForm}>
+      <h1>{PAGE_TITLES.EDIT_REVIEW}</h1>
+      <h2>{PAGE_LABELS.SHOP.NAME}：{shop.name}</h2>
+      <Form
+        onSubmit={handleEditReview}
+        handleSubmit={handleSubmit}
+        serverErrorMessages={serverErrorMessages}
+        register={register}
+        errors={errors}
+        fields={fields}
+      />
+    </div>
   );
 };
 
