1ad668d347ee81067396870b53ea6e1634cfef32
IMshirai <m.shirai@infomart.co.jp>
2025-07-25 09:33:32 +0900

    口コミ編集APIリクエスト

--- app/_utils/api/reviews.ts
+++ app/_utils/api/reviews.ts
@@ -3,33 +3,46 @@ import { REVIEWS_URL } from "./apiUrl";
 import { ReviewData } from "@/app/_interfaces/dto/response/ReviewData";
 import { ReviewFormData } from "@/app/_interfaces/dto/request/ReviewFormData";
 import { mockReviews } from "@/___tests___/mocks/data";
+import { request } from "http";
+import { responseCookiesToRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
+import { error } from "console";
 
 // 口コミ取得APIリクエスト
 export const getReviews = async (baseId?: number, limit?: number, sort?: string): Promise<ReviewData[]> => {
-  try{
+  try {
     return mockReviews;
     const response = await api.get<ReviewData[]>(REVIEWS_URL.GET(baseId, limit, sort));
     return response.data;
-  }catch(error){
+  } catch (error) {
     throw error;
   }
 };
 
 // 口コミ削除APIリクエスト
 export const deleteReview = async (reviewId: number): Promise<void> => {
-  try{
+  try {
     const response = await api.delete(REVIEWS_URL.DELETE(reviewId));
-  }catch(error){
+  } catch (error) {
     throw error;
   }
 };
 
 //口コミ投稿APIリクエスト
-export const createReview = async (reviewForm: ReviewFormData): Promise<ReviewData> =>{
-  try{
-    const response = await api.post<ReviewData>(REVIEWS_URL.CREATE , reviewForm);
+export const createReview = async (reviewForm: ReviewFormData): Promise<ReviewData> => {
+  try {
+    const response = await api.post<ReviewData>(REVIEWS_URL.CREATE, reviewForm);
     return response.data;
-  } catch (error) { 
+  } catch (error) {
     throw error;
   }
 };
+
+//口コミ編集APIリクエスト
+export const editReview = async (reviewId: number, reviewForm: ReviewFormData): Promise<ReviewData> => {
+  try {
+    const response = await api.put<ReviewData>(REVIEWS_URL.PUT(reviewId), reviewForm);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
